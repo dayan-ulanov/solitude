b5c9ed79fc475659b1503b9ff1f3eced9a380cf2
daniyar sharshenbaev <daniyar.sharshenbaev@energobank.kg>
2025-12-31 10:11:50 +0600

    chore: remove build artifacts from git and fix gitignore

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,8 +1,10 @@
+import { Button } from "@solitude/ui";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <Button title="Button" className="bg-red-600">Click</Button>
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
---
+++ packages/ui/src/components/button.tsx
@@ -0,0 +1,8 @@
+export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+  return (
+    <button
+      {...props}
+      className={`px-4 py-2 rounded-md bg-black text-white hover:bg-neutral-800 ${props.className}`}
+    >{props.children}</button>
+  );
+}
