f52fe037397ddd7b1b4a04a92f41921bd1326476
daniyar sharshenbaev <daniyar.sharshenbaev@energobank.kg>
2025-12-31 11:58:13 +0600

    fix/feat: add prettier and use & fix typescript

--- packages/ui/src/components/button.tsx
+++ packages/ui/src/components/button.tsx
@@ -3,6 +3,8 @@ export function Button(props: React.ButtonHTMLAttributes<HTMLButtonElement>) {
     <button
       {...props}
       className={`px-4 py-2 rounded-md bg-black text-white hover:bg-neutral-800 ${props.className}`}
-    >{props.children}</button>
+    >
+      {props.children}
+    </button>
   );
 }
